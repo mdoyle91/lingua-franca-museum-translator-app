d51b7b964b0f8c35f3426e6d09aad98a59473184
mdoyle91 <mike.w.doyle91@gmail.com>
2024-12-10 12:45:26 -0300

    Added URLs, Views, Templates, Static Files

---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+
+# main_app/views.py
+
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+    return render(request, 'about.html')
