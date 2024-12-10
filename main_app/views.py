b7b9b9054ae4b606b1582370eeaac09dd5962910
mdoyle91 <mike.w.doyle91@gmail.com>
2024-12-10 15:28:15 -0300

    Preliminary Homepage HTML

--- main_app/views.py
+++ main_app/views.py
@@ -4,11 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# main_app/views.py
 
 def home(request):
-    # Send a simple HTML response
-    return render(request, '<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
