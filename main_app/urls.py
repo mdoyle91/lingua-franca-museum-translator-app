0b2c585e5bedcba99ebcba0d77ab8cddc5e6fadb
mdoyle91 <mike.w.doyle91@gmail.com>
2024-12-12 12:38:52 -0300

    Created Institution Index, Extra Code That Does Not Work Yet

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('exhibits/', views.exhibit_index, name='exhibit-index'),
+    path('institutions/', views.institution_index, name='institution-index'),
+    path('institutions/<int:institution_id>/', views.institution_detail, name='institution-detail'),
     # path('institutions/<int:institution_id>/exhibits/<int:exhibit_id>', views.exhibit_detail, name='exhibit-detail'),
     #path('institutions/create/', views.InstitutionCreate.as_view(), name='institution-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Exhibit
+from .models import Exhibit, Institution
 
 
 
@@ -9,16 +9,13 @@ from .models import Exhibit
 def home(request):
     return render(request, 'home.html')
 
-def about(request): #Will remove this eventually.
-    return render(request, 'about.html')
-
 def institution_index(request):
-    # Render the cats/index.html template with the cats data
-    return render(request, 'institution/index.html', {'institutions': institutions})
+    institutions = Institution.objects.all()
+    return render(request, 'institutions/index.html', {'institutions': institutions})
 
-# def exhibit_index(request): 
-#     exhibits = Exhibit.objects.all()
-#     return render(request, 'exhibits/exhibit-index.html', {'exhibits': exhibits})
+def exhibit_index(request): 
+     exhibits = Exhibit.objects.all()
+    return render(request, 'exhibits/index.html', {'exhibits': exhibits})
 
 #  def exhibit_detail(request):  
 #     return render(request, 'exhibits/exhibit-detail.html', {'exhibits': exhibits}) #May need to alter the portin in curly brackets at the end.
