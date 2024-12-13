a854e6d665fc28c7006ae3770f6dcbc51a65e1ed
mdoyle91 <mike.w.doyle91@gmail.com>
2024-12-13 17:03:02 -0300

    Update Button Functioning, Delete Still Not Functional

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-#from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 class Institution(models.Model):
     name = models.TextField(max_length=25)
